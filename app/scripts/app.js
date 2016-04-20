8bf681639b6bc469ccc5d0cb83215f9d9b7876a8
awatson32 <audrey.watson32@gmail.com>
2016-04-20 12:25:00 -0500

    Initial commit.

---
+++ app/scripts/app.js
@@ -0,0 +1,33 @@
+'use strict';
+
+/**
+ * @ngdoc overview
+ * @name liftPortWebsiteApp
+ * @description
+ * # liftPortWebsiteApp
+ *
+ * Main module of the application.
+ */
+angular
+  .module('liftPortWebsiteApp', [
+    'ngAnimate',
+    'ngCookies',
+    'ngResource',
+    'ngRoute',
+    'ngSanitize',
+    'ngTouch'
+  ])
+  .config(function ($routeProvider) {
+    $routeProvider
+      .when('/', {
+        templateUrl: 'views/main.html',
+        controller: 'MainCtrl'
+      })
+      .when('/about', {
+        templateUrl: 'views/about.html',
+        controller: 'AboutCtrl'
+      })
+      .otherwise({
+        redirectTo: '/'
+      });
+  });
