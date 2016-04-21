cc05e510a73f0c1fdcca8e2341b5caca9ec9c69c
awatson32 <audrey.watson32@gmail.com>
2016-04-21 18:19:30 -0500

    Fixed favicon, for realz.

--- test/karma.conf.js
+++ test/karma.conf.js
@@ -29,6 +29,11 @@ module.exports = function(config) {
       'bower_components/angular-route/angular-route.js',
       'bower_components/angular-sanitize/angular-sanitize.js',
       'bower_components/angular-touch/angular-touch.js',
+      'bower_components/fullpage.js/jquery.fullPage.js',
+      'bower_components/fullpage.js/vendors/jquery.easings.min.js',
+      'bower_components/fullpage.js/vendors/jquery.slimscroll.min.js',
+      'bower_components/angular-fullpage.js/angular-fullpage.js',
+      'bower_components/angular-fullpage.js/angular-fullpage.min.js',
       'bower_components/angular-mocks/angular-mocks.js',
       // endbower
       'app/scripts/**/*.js',
