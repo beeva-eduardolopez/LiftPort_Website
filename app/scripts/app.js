c48f42c0effe325592bf3b7078a15172ad27141b
beeva-eduardolopez <eduardo.lopez.anta@bbva.com>
2016-04-21 09:31:55 -0500

    changes

--- app/scripts/app.js
+++ app/scripts/app.js
@@ -10,18 +10,28 @@
  */
 angular
   .module('liftPortWebsiteApp', [
+    // 'fullPage.js',
     'ngAnimate',
     'ngCookies',
     'ngResource',
     'ngRoute',
     'ngSanitize',
     'ngTouch'
+    
   ])
   .config(function ($routeProvider) {
     $routeProvider
       .when('/', {
         templateUrl: 'views/main.html',
         controller: 'MainCtrl'
+      })
+       .when('/test', {
+        templateUrl: 'views/test.html',
+        controller: 'TestCtrl'
+      })
+       .when('/test2', {
+        templateUrl: 'views/test2.html',
+        controller: 'TestCtrl2'
       })
       .when('/about', {
         templateUrl: 'views/about.html',
