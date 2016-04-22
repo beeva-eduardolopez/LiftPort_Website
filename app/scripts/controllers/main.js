f202261acac0c7634a48f6249773891ee1c786cd
Patrick Locke <patrick.s.locke@gmail.com>
2016-04-21 20:43:08 -0500

    Major design of Navbar. Included new logo design.

--- app/scripts/controllers/main.js
+++ app/scripts/controllers/main.js
@@ -26,9 +26,9 @@ angular.module('liftPortWebsiteApp')
         //to avoid problems with css3 transforms and fixed elements in Chrome, as detailed here: https://github.com/alvarotrigo/fullPage.js/issues/208
         css3: false
       });
-      $(".nav li a").on("click", function(){
-            $(".nav").find(".active").removeClass("active");
-            $(this).parent().addClass("active");
-      });
+    //   $(".nav li a").on("click", function(){
+    //         $(".nav").find(".active").removeClass("active");
+    //         $(this).addClass("active");
+    //   });
     });
   });
