07890e2f979cab6aec88128b61d0c1e223ff6cd4
beeva-eduardolopez <eduardo.lopez.anta@bbva.com>
2016-04-23 14:57:39 -0500

    ee

--- app/scripts/controllers/images.js
+++ app/scripts/controllers/images.js
@@ -3,44 +3,51 @@
 angular.module('liftPortWebsiteApp')
     .controller('ImagesCtrl', ['$scope', '$http', function ($scope, $http) {
 
+        var token = '';
+
         var uploadUrl = 'https://api.box.com/oauth2/token';
+        if ($scope.token == "" || $scope.token === undefined) {
+             var post = {
+                code: "BXVhS1B61iMq8TC7VodT4SHJOFXO2wQc",
+                grant_type: "authorization_code",
+                client_id: "ak3nq6j939d1i6xusk9ibfsd7lkeu1qp",
+                client_secret: "GtbGaycXZxYAAFSWJAg2oSxUYFF8l3ex"
+            };
 
-        var headers = {
-            grant_type: "authorization_code",
-            code: "EoMxJP5TGu68ygVFx4Rw2zfFJXtz79Sz",
-            client_id: "ak3nq6j939d1i6xusk9ibfsd7lkeu1qp",
-            client_secret: "GtbGaycXZxYAAFSWJAg2oSxUYFF8l3ex"
-        };
+            $.ajax({
+                url: uploadUrl,
+                type: 'POST',
+                // This prevents JQuery from trying to append the form as a querystring
+                // processData: false,
+                // contentType: false,
+                data: post
+            }).then(function (result) {
+                $scope.token = result.access_token;
+                token = result.access_token;
+            }, function (err) {
+                console.log(err);
+            });
+        } else {
 
-        $.ajax({
-            url: uploadUrl,
-            headers: headers,
-            type: 'POST',
-            // This prevents JQuery from trying to append the form as a querystring
-            // processData: false,
-            // contentType: false,
-        }).then(function (result) {
-             console.log(result.data);
-        }, function (err) {
-            console.log(err);
-        });
+        }
+          $.ajax({
+                url:  'https://api.box.com/2.0/folders/7581833161/items',
+                type: 'GET',
+                headres:{
+                    Authorization:  'Bearer '+token
+                }
+                // This prevents JQuery from trying to append the form as a querystring
+                // processData: false,
+                // contentType: false,
+                // data: post
+            }).then(function (result) {
+               console.log(result.entries);
+            }, function (err) {
+                console.log(err);
+            });
+          
+           
 
-
-        // $http({
-        //     url: 'https://api.box.com/oauth2/token',
-        //     method: 'POST',
-        //     headers: {
-        //         "grant_type": "authorization_code",
-        //         "code": "EoMxJP5TGu68ygVFx4Rw2zfFJXtz79Sz",
-        //         "client_id": "ak3nq6j939d1i6xusk9ibfsd7lkeu1qp",
-        //         "client_secret": "GtbGaycXZxYAAFSWJAg2oSxUYFF8l3ex"
-        //     }
-
-        // }).then(function (result) {
-        //     var edu = result.data;
-        // }, function (err) {
-        //     console.log(err);
-        // });
     }]);
 
 
