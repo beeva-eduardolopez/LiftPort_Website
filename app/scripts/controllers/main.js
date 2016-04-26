cc444b35c92fe931b79834f38d9a542fde21a5e2
Patrick Locke <patrick.s.locke@gmail.com>
2016-04-25 22:01:55 -0500

    Adjusted source docs for autoHideNavbar and main.js functions to get navbar to hide on page load.

--- app/scripts/controllers/main.js
+++ app/scripts/controllers/main.js
@@ -14,40 +14,39 @@ angular.module('liftPortWebsiteApp')
       'AngularJS',
       'Karma'
     ];
-   $(document).ready(function () {
-     if (typeof $.fn.fullpage.destroy == 'function') {
-       $.fn.fullpage.destroy('all');
-     }
-     $('#fullpage').fullpage({
-    //    fixedElements: '#header',
-       autoScrolling: false,
-       verticalCentered: false,
-       css3: false,
-       afterRender: function(){
-					//playing the video
-					$('video').get(0).play();
-				}
-     });
-    $('.navbar-fixed-top').autoHidingNavbar().autoHidingNavbar('setHeightOffset',100).autoHidingNavbar('hide', true);
-   });
-    // $scope.awesomeThings = [
-    //   'HTML5 Boilerplate',
-    //   'AngularJS',
-    //   'Karma'
-    // ];
-    // if (typeof $.fn.fullpage.destroy == 'function') {
-    //   $.fn.fullpage.destroy('all');
-    // }
-    // $(document).ready(function () {
-    //   if (typeof $.fn.fullpage.destroy == 'function') {
-    //     $.fn.fullpage.destroy('all');
-    //   }
-    //   $('#fullpage').fullpage({
-    //     fixedElements: '#header',
-    //     autoScrolling: false,
-    //     verticalCentered: false,
-    //     css3: false
-    //   });
-    // });
-
-  });
+    $(document).ready(function () {
+        if (typeof $.fn.fullpage.destroy == 'function') {
+        $.fn.fullpage.destroy('all');
+        }
+        $('#fullpage').fullpage({
+        //    fixedElements: '#header',
+        autoScrolling: false,
+        verticalCentered: false,
+        fitToSection: false,
+        css3: false,
+        afterRender: function(){
+                        //playing the video
+                        $('video').get(0).play();
+                    }
+        });
+        //Navbar show-hide controls. Utilizes plugin Bootstrap-AutoHidingNavbar and basic JQuery
+        $('.navbar-fixed-top').autoHidingNavbar('hide', true);
+        $('.onHidden').on('click', function(){
+            console.log('clicked!')
+            $('.navbar-fixed-top').autoHidingNavbar('show');
+        }) 
+        $(window).scroll( function() {
+            var value = $(window).scrollTop();
+                if (value === 0){
+                    $('.navbar-fixed-top').autoHidingNavbar('hide', true);
+                } else if ( value < 10 ) {
+                    console.log('top')
+                    $('.navbar-fixed-top').autoHidingNavbar('hide', true);
+                }  else {
+                    $('.navbar-fixed-top').autoHidingNavbar();
+                }
+        });
+        
+    
+    })
+});
