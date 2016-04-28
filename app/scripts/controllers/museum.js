25a1cecf1d652271b653a7bdbf270c75793fc794
awatson32 <audrey.watson32@gmail.com>
2016-04-28 07:15:54 -0500

    Added images to exhibit

--- app/scripts/controllers/museum.js
+++ app/scripts/controllers/museum.js
@@ -2,4 +2,8 @@
 
 angular.module('liftPortWebsiteApp')
     .controller('MuseumCtrl', function ($scope) {
-    });
+        
+        $scope.images = [
+            {"img1": "../images/museum/SS_SpaceElevator4.jpg"}
+        ]
+});
