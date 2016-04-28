6a3c77bca60d66f93a3545c3cc793a03e35f37e5
awatson32 <audrey.watson32@gmail.com>
2016-04-28 08:29:06 -0500

    Added remaining images to object array. Exhibit div adding additional thumbnails, though. Researching.

--- app/scripts/controllers/museum.js
+++ app/scripts/controllers/museum.js
@@ -4,6 +4,13 @@ angular.module('liftPortWebsiteApp')
     .controller('MuseumCtrl', function ($scope) {
         
         $scope.images = [
-            {"img1": "../images/museum/SS_SpaceElevator4.jpg"}
+            {"img1": "../images/museum/SS_SpaceElevator4.jpg"},
+            {"img2": "../images/museum/SS_SpaceElevator5.jpg"},
+            {"img3": "../images/museum/SS_SpaceElevator6.jpg"},
+            {"img4": "../images/museum/SS_SpaceElevator7.jpg"},
+            {"img5": "../images/museum/SS_SpaceElevator8.jpg"},
+            {"img6": "../images/museum/SS_SpaceElevator9.jpg"},
+            {"img7": "../images/museum/SS_SpaceElevator10.jpg"},
+            {"img8": "../images/museum/SS_SpaceElevator12.jpg"}
         ]
 });
