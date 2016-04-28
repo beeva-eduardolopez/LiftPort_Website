552b7308715085d84f3a05841a8b627303e36081
awatson32 <audrey.watson32@gmail.com>
2016-04-28 10:54:43 -0500

    Fixed object array gallery. Fixing modal

--- app/scripts/controllers/museum.js
+++ app/scripts/controllers/museum.js
@@ -5,13 +5,13 @@ angular.module('liftPortWebsiteApp')
         
         
         $scope.images = [
-            {"img1": "../images/museum/SS_SpaceElevator4.jpg"},
-            {"img2": "../images/museum/SS_SpaceElevator5.jpg"},
-            {"img3": "../images/museum/SS_SpaceElevator6.jpg"},
-            {"img4": "../images/museum/SS_SpaceElevator7.jpg"},
-            {"img5": "../images/museum/SS_SpaceElevator8.jpg"},
-            {"img6": "../images/museum/SS_SpaceElevator9.jpg"},
-            {"img7": "../images/museum/SS_SpaceElevator10.jpg"},
-            {"img8": "../images/museum/SS_SpaceElevator12.jpg"}
+            {"imgSrc": "../images/museum/SS_SpaceElevator4.jpg"},
+            {"imgSrc": "../images/museum/SS_SpaceElevator5.jpg"},
+            {"imgSrc": "../images/museum/SS_SpaceElevator6.jpg"},
+            {"imgSrc": "../images/museum/SS_SpaceElevator7.jpg"},
+            {"imgSrc": "../images/museum/SS_SpaceElevator8.jpg"},
+            {"imgSrc": "../images/museum/SS_SpaceElevator9.jpg"},
+            {"imgSrc": "../images/museum/SS_SpaceElevator10.jpg"},
+            {"imgSrc": "../images/museum/SS_SpaceElevator12.jpg"}
         ]
 });
